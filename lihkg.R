5a9c689d2443d740584cc162f7592d86db59a1da
高紹芳｜Christine Shào-Fāng Kāo <christinesfkao@gmail.com>
2020-11-24 19:19:27 +0800

    Update lihkg.R
    debug: adding strings in R with paste()

--- lihkg.R
+++ lihkg.R
@@ -1,21 +1,22 @@
-install.packages("LIHKGr")
-install.packages("rio")
+#install.packages("LIHKGr")
+#install.packages("rio")
 library(LIHKGr)
 library(rio)
 
 ## Creating a Firefox instance with a random port.
 keyword <- commandArgs(trailingOnly = TRUE)
-threadids <- as.vector(read.csv(keyword +"_links_id.txt"))
-print("The keyword you selected is \"" + keyword + "\", with " +length(threadids[[1]]) + "threads.")
+filename <- paste(keyword, "links.txt", sep = "")
+threadids <- read.table(filename)[[1]]
+paste("The keyword you selected is \'", keyword, "\', with ", length(threadids), " threads.", sep = "")
 lihkg <- create_lihkg(browser = "firefox", port = sample(10000:60000, 1), verbose = FALSE)
 
-print("Preparing for scrapping at " + date())
-lihkg$scrape(threadids$V1)
+paste("Preparing for scrapping at" , date())
+lihkg$scrape(threadids)
 print(head(lihkg$bag))
 
 print("Exporting scrapped contents...")
 rio::export(lihkg$bag, keyword + "_contents.xlsx")
 
 lihkg$finalize()
-print("End of program at " + data())
+print("End of program at" , date())
 
